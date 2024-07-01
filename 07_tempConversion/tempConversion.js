a1a3c8a9547f84a6ab71715e20717a2e5ab60f80
Alex Reilly <iamalexreilly@gmail.com>
2024-07-01 15:46:45 -0400

    Solve javscript exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = ((temp-32)*5/9);
+  if (result %1 !== 0){
+    return +result.toFixed(1);
+  }
+  else{
+    return +result;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = ((temp*9/5)+32);
+  if (result %1 !== 0){
+    return +result.toFixed(1);
+  }
+  else{
+    return +result;
+  }
 };
 
 // Do not edit below this line
