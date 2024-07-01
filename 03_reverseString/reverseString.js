9964c036760ae2e079b2689eca16406e6542608a
Alex Reilly <iamalexreilly@gmail.com>
2024-07-01 11:57:55 -0400

    Solve exercise 3 in javascript exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArr = string.split("");
+    stringArr.reverse();
+    return stringArr.join("");
 };
 
 // Do not edit below this line
